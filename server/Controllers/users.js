afd2f1eb21142857718412c05b2cbb07e080926f
Maiky33 <maicolbarrerag@gmail.com>
2024-09-19 11:10:38 -0500

    token configuration

--- server/Controllers/users.js
+++ server/Controllers/users.js
@@ -30,7 +30,6 @@ export const register = async(req, res)=>{
       id:userSaved._id
     })
 
-    console.log('token al crearse',token)
 
     const cookieOptions = {
       httpOnly: true,
@@ -42,7 +41,6 @@ export const register = async(req, res)=>{
     //lo guardamos en una cookie
     res.cookie('token', token, cookieOptions);
 
-    console.log('respuesta del res.cookie',res.cookie('token', token, cookieOptions))
     //devolvemos al frontend el user sin la password
     return res.json({  
       id:userSaved._id,
@@ -84,6 +82,9 @@ export const login = async(req, res)=> {
       id:UserFound._id
     })
 
+    console.log('token al crearse',token)
+
+
     const cookieOptions = {
       httpOnly: true,
       secure: true, 
@@ -94,7 +95,10 @@ export const login = async(req, res)=> {
     
 
     //lo guardamos en una cookie
-    res.cookie("token", token, cookieOptions);
+    const rescoo = res.cookie("token", token, cookieOptions);
+
+    console.log('respuesta del res.cookie',rescoo)
+
 
     //devolvemos al frontend el user sin la password
     res.json({  
