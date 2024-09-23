74bf2aba1db239e9811ad49e03be8eba58722e6d
Maiky33 <maicolbarrerag@gmail.com>
2024-09-23 18:09:18 -0500

    updated project vercel

--- server/middlewares/validateToken.js
+++ server/middlewares/validateToken.js
@@ -4,6 +4,8 @@ import { TOKEN_SECRET } from '../config.js';
 export const authRequired = (req, res, next) => {   
   
   const authHeader = req.headers['authorization'];
+
+  console.log("authHeader",authHeader)
     
   if (!authHeader) return res.status(401).json({ message: "No token, authorization denied" });
 
