0a7ee0962ba66cbb20c1c948653dcd7d462fc563
Maiky33 <maicolbarrerag@gmail.com>
2024-09-23 17:58:00 -0500

    updated project vercel

--- server/middlewares/validateToken.js
+++ server/middlewares/validateToken.js
@@ -1,31 +1,21 @@
-import jwt from 'jsonwebtoken'
-import {TOKEN_SECRET} from '../config.js'
+import jwt from 'jsonwebtoken';
+import { TOKEN_SECRET } from '../config.js';
 
 export const authRequired = (req, res, next) => {   
-    const cookieHeader = req.headers.cookie;
   
-    if (cookieHeader) {
-      const cookies = cookieHeader.split('; ').reduce((acc, cookie) => {
-        const [name, value] = cookie.split('=');
-        acc[name] = value;
-        return acc;
-      }, {});
-  
-      const token = cookies.token;
-  
-      // Si no hay token, responder con 401
-      if (!token) return res.status(401).json({ message: "No token, authorization denied" });
-  
-      jwt.verify(token, TOKEN_SECRET, (err, user) => {  
-        // Si hay algún error, responder con 403
-        if (err) return res.status(403).json({ message: "Invalid token" });
-  
-        // Si todo está bien, añadir el usuario al req y pasar al siguiente middleware
-        req.user = user;
-        next();
-      });
-    } else {
-      // Si no hay cabecera de cookies, responder con 401
-      return res.status(401).json({ message: "No token, authorization denied" });
-    }
-  };
+  const authHeader = req.headers['authorization'];
+    
+  if (!authHeader) return res.status(401).json({ message: "No token, authorization denied" });
+
+    const token = authHeader.split(' ')[1];
+    
+    if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+
+    jwt.verify(token, TOKEN_SECRET, (err, user) => {  
+       
+      if (err) return res.status(403).json({ message: "Invalid token" });
+ 
+      req.user = user;
+      next();
+    });
+};
