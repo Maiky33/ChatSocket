639569665d596eb5ca59bc675cc6186bb40dfff9
Maiky33 <maicolbarrerag@gmail.com>
2024-09-26 12:56:41 -0500

    delete headers

--- server/middlewares/validateToken.js
+++ server/middlewares/validateToken.js
@@ -1,23 +1,23 @@
 import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from '../config.js';
 
-export const authRequired = (req, res, next) => {   
+export const authRequired = async(req, res, next) => {   
   
-  const authHeader = req.headers['authorization'];
+  const authHeader = await req.headers['authorization'];
     
   if (!authHeader) return res.status(401).json({ message: "No authHeader" });
 
-    const token = authHeader.split(' ')[1];
+  const token = authHeader?.split(' ')[1];
 
-    console.log("token",token)
-    
-    if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+  console.log("token",token)
+  
+  if (!token) return res.status(401).json({ message: "No token, authorization denied" });
 
-    jwt.verify(token, TOKEN_SECRET, (err, user) => {  
-       
-      if (err) return res.status(403).json({ message: "Invalid token" });
- 
-      req.user = user;
-      next();
-    });
+  jwt.verify(token, TOKEN_SECRET, (err, user) => {  
+      
+    if (err) return res.status(403).json({ message: "Invalid token" });
+
+    req.user = user;
+    next();
+  });
 };
